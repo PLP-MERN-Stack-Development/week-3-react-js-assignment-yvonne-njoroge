20c27780ebd4d4f94a62cc507b704dcd259eb97c
Yvonne Njoroge <ynjoroge@chlorideexide.com>
2025-07-06 23:53:11 +0300

    Base created

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base : '/week-3-react-js-assignment-yvonne-njoroge/',
   plugins: [react()],
 })
